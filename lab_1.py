e78eed027de22145733898355f57ff86420a7307
sibteali786 <sibteali786@gmail.com>
2022-05-01 17:07:56 +0500

    added important tasks of custom functions i.e Task 4

--- lab_1.py
+++ lab_1.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-def blackAndWhite(imageSize,verticaLines):
+def VertcialLineImage(imageSize,verticaLines):
     img = np.zeros([imageSize,imageSize])
     img.fill(0)
     for t in range(1,verticaLines,2):
@@ -8,7 +8,19 @@ def blackAndWhite(imageSize,verticaLines):
             for j in range((imageSize//verticaLines)*t,(imageSize//verticaLines)*(t+1)):
                 img[i][j] = 255
     return img
-img = blackAndWhite(7*49,7)  # the size of image should be integral multiple of vertical lines
+img = VertcialLineImage(7*49,7)  # the size of image should be integral multiple of vertical lines
 cv.imshow("VerticalLines",img)
-cv.waitKey()
 
+
+def boxImage(imageSize,noOfBoxes):
+    img = np.zeros([imageSize, imageSize])
+    img.fill(0)
+    for u in range(0,noOfBoxes,2):
+        for v in range(0,noOfBoxes,2):
+            for i in range(v*(imageSize//noOfBoxes),(v+1)*(imageSize//noOfBoxes)):
+                for j in range(u*(imageSize//noOfBoxes),(u+1)*(imageSize//noOfBoxes)):
+                    img[i][j] = 255
+    return img
+imgVertHori = boxImage(400,10);
+cv.imshow("Vertical And Horizontal Lines",imgVertHori)
+cv.waitKey()
